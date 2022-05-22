5f24621d400586d2662e72e4912c4be7cf5e757f
Muoniovaara Manne <manne.muoniovaara@digia.com>
2022-05-22 09:53:20 +0300

    Modify environment variables

--- common/env.ts
+++ common/env.ts
@@ -9,82 +9,86 @@ U _____ u _   _  __     __
 */
 
 //ENV
-import { config, DotenvConfig } from "https://deno.land/x/dotenv/mod.ts";
+import { config, DotenvConfig } from "https://deno.land/x/dotenv@v3.2.0/mod.ts";
 
 //LOGGER
 import * as logger from "./log.ts";
 
 export interface DbEnv {
-  MONGO_SERVER: string;
-  UN: string;
-  PW: string;
-  DB: string;
+	MONGO_SERVER: string;
+	UN: string;
+	PW: string;
+	DB: string;
+	HEROKUAPP_URL?: string;
 }
 
 export interface Env extends DbEnv {
-  LOCAL: boolean;
+	LOCAL: boolean;
 }
 
 export type DbEnvErrors = Array<keyof DbEnv>;
 
 function createEmptyDBEnv(): DbEnv {
-  return { MONGO_SERVER: "", UN: "", PW: "", DB: "" };
+	return { MONGO_SERVER: "", UN: "", PW: "", DB: "" };
 }
 export function parseDBEnv(): DbEnv {
-  const errors: DbEnvErrors = [];
-  let env: DotenvConfig | DbEnv = config();
-  if (env.MONGO_SERVER === undefined) errors.push("MONGO_SERVER");
-  if (env.UN === undefined) errors.push("UN");
-  if (env.PW === undefined) errors.push("PW");
-  if (env.DB === undefined) errors.push("DB");
-  if (errors.length > 0) {
-    logger.cyan(
-      "❌ Missing following properties from .env: " + errors.join(),
-    );
-    logger.cyan(
-      "❎ Parsing global ENVIRONMENT variables (Heroku)",
-    ), errors.splice(0, errors.length);
-    env = createEmptyDBEnv();
-    let denoEnv = Deno.env.toObject();
-    if (Object.prototype.hasOwnProperty.call(denoEnv, "MONGO_SERVER")) {
-      env.MONGO_SERVER = denoEnv.SERVER;
-    } else errors.push("MONGO_SERVER");
-    if (Object.prototype.hasOwnProperty.call(denoEnv, "UN")) {
-      env.UN = denoEnv.UN;
-    } else errors.push("UN");
-    if (Object.prototype.hasOwnProperty.call(denoEnv, "PW")) {
-      env.PW = denoEnv.PW;
-    } else errors.push("PW");
-    if (Object.prototype.hasOwnProperty.call(denoEnv, "DB")) {
-      env.DB = denoEnv.DB;
-    } else errors.push("DB");
-    if (errors.length > 0) {
-      logger.bgRed(
-        "❌ Missing following properties from global ENVIRONMENT variables (Heroku): " +
-          errors.join(),
-      );
-      logger.bgRed("❌ Fix your MongoDB configuration.");
-      logger.blue("☠ Exiting application...");
-      Deno.exit(0);
-    }
-  }
-  logger.green("✔ Database environment variables ok!");
-  return { MONGO_SERVER: env.MONGO_SERVER, UN: env.UN, PW: env.PW, DB: env.DB };
+	const errors: DbEnvErrors = [];
+	let env: DbEnv = createEmptyDBEnv();
+
+	try {
+		env = config() as unknown as DbEnv;
+	} catch (err) {
+		logger.bgRed("❌ Not using ENV file: " + err);
+		env = Deno.env.toObject() as unknown as DbEnv;
+	}
+
+	if (env.MONGO_SERVER === undefined) errors.push("MONGO_SERVER");
+	if (env.UN === undefined) errors.push("UN");
+	if (env.PW === undefined) errors.push("PW");
+	if (env.DB === undefined) errors.push("DB");
+	if (errors.length > 0) {
+		logger.cyan(
+			"❌ Missing following properties from .env: " + errors.join(),
+		);
+		logger.cyan(
+			"❎ Parsing global ENVIRONMENT variables (Heroku)",
+		), errors.splice(0, errors.length);
+		if (!Object.prototype.hasOwnProperty.call(env, "MONGO_SERVER")) errors.push("MONGO_SERVER");
+		if (!Object.prototype.hasOwnProperty.call(env, "UN")) errors.push("UN");
+		if (!Object.prototype.hasOwnProperty.call(env, "PW")) errors.push("PW");
+		if (!Object.prototype.hasOwnProperty.call(env, "DB")) errors.push("DB");
+		if (errors.length > 0) {
+			logger.bgRed(
+				"❌ Missing following properties from global ENVIRONMENT variables (Heroku): " +
+				errors.join(),
+			);
+			logger.bgRed("❌ Fix your MongoDB configuration.");
+			logger.blue("☠ Exiting application...");
+			Deno.exit(0);
+		}
+	}
+	logger.green("✔ Database environment variables ok!");
+	return { MONGO_SERVER: env.MONGO_SERVER, UN: env.UN, PW: env.PW, DB: env.DB };
 }
 export function isLocal(): boolean | undefined {
-  const env: DotenvConfig = config();
-  if (Object.prototype.hasOwnProperty.call(env, "LOCAL")) {
-    return (env.LOCAL === "true") ? true : false;
-  }
-  return;
+	try {
+		const env: DotenvConfig = config();
+		if (Object.prototype.hasOwnProperty.call(env, "LOCAL")) {
+			return (env.LOCAL === "true") ? true : false;
+		}
+	} catch (err) {
+		logger.bgRed("❌ Not using ENV file: " + err);
+		return false;
+	}
+	return;
 }
 export function herokuUrl(): string | undefined {
-  let denoEnv = Deno.env.toObject();
-  if (Object.prototype.hasOwnProperty.call(denoEnv, "HEROKUAPP_URL")) {
-    return denoEnv.HEROKUAPP_URL;
-  }
-  return;
+	const env = Deno.env.toObject() as unknown as DbEnv;
+	if (Object.prototype.hasOwnProperty.call(env, "HEROKUAPP_URL")) {
+		return env.HEROKUAPP_URL;
+	}
+	return;
 }
 export function dbLocal(): string | undefined {
-  return config().DB_LOCAL;
+	return config().DB_LOCAL;
 }
